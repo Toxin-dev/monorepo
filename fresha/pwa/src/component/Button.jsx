163cbda4b952b1a29a323622617288bb37190574
rakshyak <rakshak1998@gmail.com>
2022-12-01 10:58:53 +0530

    created loading state for button.

--- fresha/pwa/src/component/Button.jsx
+++ fresha/pwa/src/component/Button.jsx
@@ -1,8 +1,14 @@
 import propTypes from "prop-types";
-export function Button({className,textContent}) {
-  return <button className={className}>{textContent}</button>;
+export function Button({ className, textContent }) {
+  return <button className={className}>{true ? textContent : <Loading />} </button>;
 }
 
 Button.prototype = {
   textContent: propTypes.string.isRequired,
 };
+
+
+function Loading() {
+  return <div className="loading">
+    <span className="circle"></span><span className="circle"></span><span className="circle"></span></div>
+}
